c22312ff39e1b78714746ea30673664a434b5519
aristeap <95976465+aristeap@users.noreply.github.com>
2023-05-19 21:24:52 +0300

    Update PayUp.java
    Προς το παρόν θα ανοίγουμε τις οθόνες από τη main μέχρι να προσθέσουμε κάποιον actionListener για να εμφανίζονται μετά από πάτημα αντίστοιχων κουμπιών.

--- Pay_Up/src/PayUp.java
+++ Pay_Up/src/PayUp.java
@@ -2,10 +2,19 @@
 public class PayUp {
 
 	public static void main(String[] args) {
-		LogInPage login = new LogInPage();
-		SignUpPage signup = new SignUpPage();
-		AddBankAccount addbank = new AddBankAccount();
-
+		//LogInPage login = new LogInPage();
+		//SignUpPage signup = new SignUpPage();
+		//AddBankAccount addbank = new AddBankAccount();
+		//AddElectricBill addElecBill = new AddElectricBill();
+		//AddInstallment addInstal  = new AddInstallment();		
+		//AddPhoneBill addPhoneBill = new  AddPhoneBill(); 
+		//AddWaterBill addWatBill = new  AddWaterBill();  
+		//PayElectricBill payElecBill = new PayElectricBill();
+		//PayInstallment payInstal = new PayInstallment();
+		//PayPhoneBill payPhoBill = new PayPhoneBill();
+		//PayWaterBill payWatBill  = new PayWaterBill();
+		IndexPage indexpage = new IndexPage();	
+		
 	}
 
 }
