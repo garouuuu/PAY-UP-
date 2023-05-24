4aaddfe17d525263210477223efb296b60bfd111
aristeap <95976465+aristeap@users.noreply.github.com>
2023-05-24 15:34:51 +0300

    Add files via upload
    Πρόσθεσα και ένα πεδίο για username που έπρεπε να είχε μπει από την αρχή αφού στη LogInPage ζητάμε username και password.

--- Pay_Up/src/SignUpPage.java
+++ Pay_Up/src/SignUpPage.java
@@ -15,6 +15,7 @@ public class SignUpPage
 	static OurLabel windowLabel;	
 	static OurLabel fNameLabel;
 	static OurLabel lNameLabel;
+	static OurLabel usernameLabel;
 	static OurLabel emailLabel;
 	static OurLabel passwordLabel;
 	static OurLabel confirmPasLabel;
@@ -23,6 +24,7 @@ public class SignUpPage
 	static OurTextField emailText;
 	static OurTextField passwordText ;
 	static OurTextField confirmPasText ;	
+	static OurTextField usernameText;
 	static OurButton nextButton  ;
 	static OurButton backButton ;
 
@@ -35,24 +37,32 @@ public class SignUpPage
 		windowLabel = new OurLabel("Συμπλήρωσε τα στοιχεία σου",40,5,350,100,new Font("Verdana", Font.BOLD, 20));
 		fNameLabel = new OurLabel("Όνομα",20, 100, 130, 40,new Font("Verdana", Font.BOLD, 14));		
 		lNameLabel = new OurLabel("Επίθετο",200, 100, 130, 40,new Font("Verdana", Font.BOLD, 14));
-		emailLabel = new OurLabel("Email",20, 200, 130, 40,new Font("Verdana", Font.BOLD, 14));
-		passwordLabel = new OurLabel("Κωδικός",20, 300, 130, 40,new Font("Verdana", Font.BOLD, 14));
-		confirmPasLabel  = new OurLabel("Επιβεβαίωση Κωδικού",20, 400, 200, 40,new Font("Verdana", Font.BOLD, 14));
+		
+		usernameLabel = new OurLabel("Όνομα Χρήστη",20, 180, 130, 40,new Font("Verdana", Font.BOLD, 14));
+		
+		emailLabel = new OurLabel("Email",20, 260, 160, 40,new Font("Verdana", Font.BOLD, 14));
+		passwordLabel = new OurLabel("Κωδικός",20, 360, 130, 40,new Font("Verdana", Font.BOLD, 14));
+		confirmPasLabel  = new OurLabel("Επιβεβαίωση Κωδικού",20, 460, 200, 40,new Font("Verdana", Font.BOLD, 14));
 		
 		fNameText = new OurTextField(20, 130, 160, 30);
 		lNameText = new OurTextField(200, 130, 170, 30);
-		emailText = new OurTextField(20, 230, 350, 30);
-		passwordText = new OurTextField(20, 330, 350, 30);
-		confirmPasText = new OurTextField(20, 430, 350, 30);
 		
-		nextButton = new OurButton("Επόμενο",280, 530, 90, 40,new Color(0, 204, 102));
-		backButton = new OurButton("Πίσω",20, 530, 90, 40,new Color(0, 128, 255));
+		usernameText = new OurTextField(20, 210, 350, 30);
+		
+		emailText = new OurTextField(20, 290, 350, 30);
+		passwordText = new OurTextField(20, 390, 350, 30);
+		confirmPasText = new OurTextField(20, 490, 350, 30);
+		
+		nextButton = new OurButton("Επόμενο",280, 560, 90, 40,new Color(0, 204, 102));
+		backButton = new OurButton("Πίσω",20, 560, 90, 40,new Color(0, 128, 255));
     	
 		
 		
 		frame.add(windowLabel);
 		frame.add(fNameLabel);
     	frame.add(lNameLabel);
+    	frame.add(usernameLabel);
+    	frame.add(usernameText);
     	frame.add(emailLabel);
     	frame.add(passwordLabel);
     	frame.add(confirmPasLabel);	
